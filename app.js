8eae332740b340559e0cd9d49e50a8b4e4833a0d
Scott <ScottAdelman97@gmail.com>
2022-08-09 10:51:21 -0600

    Added functionality to functions

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
-
-
 function printOdds(count) {
     var count = prompt("What is you number?")
     if (count < 0) count = count * -1;
@@ -15,6 +13,7 @@ function printOdds(count) {
     }
   }
   console.log(printOdds()); 
+
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
@@ -32,8 +31,9 @@ function checkAge(age) {
 
   console.log(checkAge());
 
+
 // Exercise 3 Section
-console.log("EXERCISE 2:\n==========\n");
+console.log("EXERCISE 3:\n==========\n");
 
 function checkPlane(x, y) {
     var x = prompt("What is your x-coordinate?")
@@ -62,8 +62,10 @@ function checkPlane(x, y) {
     }
 }
 console.log(checkPlane());
+
+
 // Exercise 4 Section
-console.log("EXERCISE 2:\n==========\n");
+console.log("EXERCISE 4:\n==========\n");
 
 function triangleType(a, b, c) {
 
@@ -87,5 +89,34 @@ function triangleType(a, b, c) {
         console.log("Isosceles Traingle");
     }
 }
-
 console.log(triangleType());
+
+
+// Exercise 5 Section
+console.log("EXERCISE 5:\n==========\n");
+
+function dataPlan(planLimit, day, usage){
+    var planLimit = prompt("Please enter in the amount of data in the plan per 30 day period");
+    var day = prompt("Please enter in the current day in the 30 day period");
+    var usage = prompt("Please enter in the total amount of data used so far");
+    var daysRemaining = 30 - day;
+    var dailyUsagePlan = planLimit / 30; 
+    var actualDailyUsage = usage / day; 
+    var projectedUsage = actualDailyUsage * 30; 
+    var dataExceededBy = projectedUsage - planLimit;
+    var dataRemaining = planLimit - usage;
+    var newUsagePlan = dataRemaining / daysRemaining; 
+
+    console.log(`${day} used, ${daysRemaining} days remaining`);
+    console.log(`Advised average daily use: ${dailyUsagePlan}`);
+
+    if(actualDailyUsage > dailyUsagePlan){
+        console.log(`You are EXCEEDING your average daily use of ${dailyUsagePlan} by ${actualDailyUsage - dailyUsagePlan} GB/day... 
+        continuing this high usage, you'll exceed your data plan by ${dataExceededBy}. To stay below your data plan, use no more than
+        ${newUsagePlan} GB/day`);
+    }
+    else if (dailyUsagePlan > actualDailyUsage){
+        console.log("You gucccci my dude. Keep using what you are using!");
+    }
+}
+console.log(dataPlan());
